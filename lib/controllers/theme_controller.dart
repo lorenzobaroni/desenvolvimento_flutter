5ae272ebd1c1a9cea57158c6c7444bceab24bf2a
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-10-14 18:12:59 -0300

    feat: cacheamento

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -1,13 +1,24 @@
 import 'package:desenvolvimento_flutter_iniciante/themes/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends ChangeNotifier {
+  final SharedPreferences sharedPreferences;
   bool isLightTheme = true;
+
+  ThemeController({required this.sharedPreferences});
 
   ThemeData get currentTheme => isLightTheme ? lightTheme : darkTheme;
 
-  void toggleTheme() {
+  void toggleTheme() async {
     isLightTheme = !isLightTheme;
+    await sharedPreferences.setBool('isLightTheme', isLightTheme);
+    notifyListeners();
+  }
+
+    Future<void> loadThemePreference() async {
+    final isLight = sharedPreferences.getBool('isLightTheme') ?? true;
+    isLightTheme = isLight;
     notifyListeners();
   }
 }
--- lib/dependencies/injector.dart
+++ lib/dependencies/injector.dart
@@ -1,8 +1,11 @@
 import 'package:desenvolvimento_flutter_iniciante/controllers/pessoa_controller.dart';
 import 'package:desenvolvimento_flutter_iniciante/controllers/theme_controller.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void injector() {
+Future<void> injector() async {
   GetIt.instance.registerSingleton<PessoaController>(PessoaController());
-  GetIt.instance.registerSingleton<ThemeController>(ThemeController());
+  GetIt.instance.registerSingleton<ThemeController>(ThemeController(
+    sharedPreferences: await SharedPreferences.getInstance()
+  ));
 }
